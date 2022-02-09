911ff2d1354485502eba8c71f0a9267ae65d69cd
tykatyk <tykatyk@gmail.com>
2022-02-09 10:11:05 +0200

    Refactor drag controls

--- js/THREEx.DragControls.js
+++ js/THREEx.DragControls.js
@@ -5,13 +5,11 @@ THREEx.DragControls = function (
   itemScene,
   viewportScene,
   viewportCamera,
-  viewportWidth,
   domElement
 ) {
   this.itemScene = itemScene;
   this.viewportScene = viewportScene;
   this.viewportCamera = viewportCamera;
-  this.viewportWidth = viewportWidth;
   this.domElement = domElement;
 
   this.states = { IDLE: -1, ACTIVE: 0, ADDED: 1 };
@@ -24,15 +22,17 @@ THREEx.DragControls = function (
     if (this.state === this.states.IDLE) return;
     if (this.dragPosition.x <= 0) return;
 
-    let x = (this.dragPosition.x / viewportWidth) * 2 - 1;
-    let y = -(this.dragPosition.y / window.innerHeight) * 2 + 1;
-    let z = 0.5;
+    const viewportWidth = document.getElementById("viewport").offsetWidth;
+
+    const x = (this.dragPosition.x / viewportWidth) * 2 - 1;
+    const y = -(this.dragPosition.y / window.innerHeight) * 2 + 1;
+    const z = 0.5;
 
     let vectorMouse = new THREE.Vector3(x, y, z);
     vectorMouse.unproject(this.viewportCamera);
     vectorMouse.sub(this.viewportCamera.position).normalize();
 
-    let distance = this.viewportCamera.position.z / vectorMouse.z;
+    const distance = this.viewportCamera.position.z / vectorMouse.z;
 
     let position = new THREE.Vector3();
     position
