49085d1dc03dd7d3fe7ba2e395a2bb5c5e2f78c0
tykatyk <tykatyk@gmail.com>
2022-02-09 10:10:23 +0200

    Refactor sidebar

--- js/THREEx.Sidebar.js
+++ js/THREEx.Sidebar.js
@@ -1,17 +1,22 @@
-let sidebarRenderer;
-let canvas;
-let scenes = [];
-let dragHandlers = [];
+/** @namespace */
+var THREEx = THREEx || {};
 
-function sidebar(mainScene, mainCamera, viewportWidth) {
-  init();
-  animate();
+THREEx.Sidebar = function (viewportScene, viewportCamera) {
+  this.viewportScene = viewportScene;
+  this.viewportCamera = viewportCamera;
+  this.scenes = [];
 
-  function init() {
+  let canvas;
+  let renderer;
+  let dragHandlers = [];
+
+  const scope = this;
+
+  this.init = function () {
     const sidebar = document.getElementById("sidebar");
     canvas = document.getElementById("canvas");
 
-    sidebarRenderer = new THREE.WebGLRenderer({
+    renderer = new THREE.WebGLRenderer({
       canvas: canvas,
       antialias: true,
     });
@@ -44,51 +49,49 @@ function sidebar(mainScene, mainCamera, viewportWidth) {
 
       const dragHandler = new THREEx.DragControls(
         scene,
-        mainScene,
-        mainCamera,
-        viewportWidth,
+        this.viewportScene,
+        this.viewportCamera,
         sceneElement
       );
 
       scene.add(element);
-      scenes.push(scene);
+      this.scenes.push(scene);
       dragHandlers.push(dragHandler);
     });
-  }
+  };
 
   function updateSize() {
     const width = canvas.clientWidth;
     const height = canvas.clientHeight;
 
     if (canvas.width !== width || canvas.height !== height) {
-      sidebarRenderer.setSize(width, height, false);
+      renderer.setSize(width, height, false);
     }
   }
 
-  function animate() {
-    requestAnimationFrame(animate);
+  this.animate = function () {
     render();
     update();
-  }
+  };
 
   function render() {
     updateSize();
-    sidebarRenderer.setClearColor(0xffffff);
-    sidebarRenderer.setScissorTest(false);
-    sidebarRenderer.clear();
-    sidebarRenderer.setClearColor(0xe0e0e0);
-    sidebarRenderer.setScissorTest(true);
+    renderer.setClearColor(0xffffff);
+    renderer.setScissorTest(false);
+    renderer.clear();
+    renderer.setClearColor(0xe0e0e0);
+    renderer.setScissorTest(true);
 
-    scenes.forEach((scene) => {
+    scope.scenes.forEach((scene) => {
       const element = scene.userData.element;
       const rect = element.getBoundingClientRect();
 
       // check if it's offscreen. If so skip it
       if (
         rect.bottom < 0 ||
-        rect.top > sidebarRenderer.domElement.clientHeight ||
+        rect.top > renderer.domElement.clientHeight ||
         rect.right < 0 ||
-        rect.left > sidebarRenderer.domElement.clientWidth
+        rect.left > renderer.domElement.clientWidth
       ) {
         return; // it's off screen
       }
@@ -97,14 +100,14 @@ function sidebar(mainScene, mainCamera, viewportWidth) {
       const width = rect.right - rect.left;
       const height = rect.bottom - rect.top;
       const left = rect.left;
-      const bottom = sidebarRenderer.domElement.clientHeight - rect.bottom;
+      const bottom = renderer.domElement.clientHeight - rect.bottom;
 
-      sidebarRenderer.setViewport(left, bottom, width, height);
-      sidebarRenderer.setScissor(left, bottom, width, height);
+      renderer.setViewport(left, bottom, width, height);
+      renderer.setScissor(left, bottom, width, height);
 
       const camera = scene.userData.camera;
 
-      sidebarRenderer.render(scene, camera);
+      renderer.render(scene, camera);
     });
   }
 
@@ -113,4 +116,4 @@ function sidebar(mainScene, mainCamera, viewportWidth) {
       handler.update();
     });
   }
-}
+};
